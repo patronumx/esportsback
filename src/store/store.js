abbfec751e0f0ad17a2f65ae9738f82f42557526
patronumx <patronumgg@gmail.com>
2026-01-07 08:32:22 +0500

    fix: remove stale root src directory to force client build

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import mapDropReducer from './slices/mapDropSlice';
-
-export const store = configureStore({
-    reducer: {
-        mapDrop: mapDropReducer,
-    },
-});
